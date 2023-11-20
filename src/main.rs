366d473b5120307bcc9533bfecae1d049471164d
cristalcorp <cristal@cristalcorp.com>
2023-11-20 13:15:16 +0100

    user input as a function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::stdin;
+
+fn what_is_your_name() -> String {
+    let mut user_input = String::new();
+    stdin()
+        .read_line(&mut user_input)
+        .expect("Cannot read line");
+    user_input.trim().to_lowercase()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello mofo, what is your name ?");
+    let name = what_is_your_name();
+    println!("hello {}", &name);
 }
