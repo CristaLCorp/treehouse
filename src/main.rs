2b8464c230fd5465ea5d7dc8619cb842526a8757
cristalcorp <cristal@cristalcorp.com>
2023-11-20 16:51:37 +0100

    keep going

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,24 @@ fn what_is_your_name() -> String {
     user_input.trim().to_lowercase()
 }
 
+fn can_get_in(name: &str) -> bool {
+    let visitor_list = ["jean", "paul", "cul"];
+    let mut can_get_in: bool = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            can_get_in = true;
+        }
+    }
+    can_get_in
+}
+
 fn main() {
     println!("Hello mofo, what is your name ?");
     let name = what_is_your_name();
-    println!("hello {}", &name);
+
+    if can_get_in(&name) {
+        println!("you are in {} !", &name);
+    } else {
+        println!("fuck off you mofo !");
+    }
 }
